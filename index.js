b910b8a4cf10c60ba3db382cff969b5624b1c6a7
blessykarunya <blessykarunyajohnson@gmail>
2022-03-28 21:41:16 +0530

    JavaScript

---
+++ index.js
@@ -0,0 +1,9 @@
+const navbarToggle = navbar.querySelector('#navbar-toggle');
+let isNavbarExpanded = navbarToggle.getAttribute('aria-expanded') === 'true';
+
+const toggleNavbarVisibility = () => {
+  isNavbarExpanded = !isNavbarExpanded;
+  navbarToggle.setAttribute('aria-expanded', isNavbarExpanded);
+};
+
+navbarToggle.addEventListener('click', toggleNavbarVisibility);
