00e550d3b28ca72e12b41db1b291bdc0582d09b1
blessykarunya <blessykarunyajohnson@gmail.com>
2022-06-12 18:16:59 +0530

    travel agents page

--- travelagents-jquery.js
+++ travelagents-jquery.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
-    $("#flip1").click(function() {
-        $("#panel1").toggle(150);
-    });
-    $("#flip2").click(function() {
-        $("#panel2").toggle(150);
-    });
-    $("#flip3").click(function() {
-        $("#panel3").toggle(150);
-    });
-    $("#flip4").click(function() {
-        $("#panel4").toggle(150);
-    });
-    $("#flip5").click(function() {
-        $("#panel5").toggle(150);
-    });
-    $("#flip6").click(function() {
-        $("#panel6").toggle(150);
-    });
-    $("#flip7").click(function() {
-        $("#panel7").toggle(150);
-    });
+	$("#flip1").click(function() {
+		$("#panel1").toggle(150);
+	});
+	$("#flip2").click(function() {
+		$("#panel2").toggle(150);
+	});
+	$("#flip3").click(function() {
+		$("#panel3").toggle(150);
+	});
+	$("#flip4").click(function() {
+		$("#panel4").toggle(150);
+	});
+	$("#flip5").click(function() {
+		$("#panel5").toggle(150);
+	});
+	$("#flip6").click(function() {
+		$("#panel6").toggle(150);
+	});
+	$("#flip7").click(function() {
+		$("#panel7").toggle(150);
+	});
 });
